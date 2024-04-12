3b41c4c2fd94365e81fd2fc42ab6563309939da5
Yiwan1w <143529072+Yiwan1w@users.noreply.github.com>
2024-04-11 22:58:17 -0400

    usagi

---
+++ exercise-7/script.js
@@ -0,0 +1,29 @@
+let usagi = document.getElementById('lotusagi');
+
+function letUsagiBack(){
+    usagi.style.backgroundImage = 'url(assets/original.jpg)'
+}
+
+function letUsagiEat(){
+    usagi.style.backgroundImage = 'url(assets/pudding.jpg)'
+}
+
+function letUsagiHot(){
+    usagi.style.backgroundImage ='url(assets/hot.jpg)'
+}
+
+function letUsagiflip(){
+    usagi.style.backgroundImage ='url(assets/flip.jpg)'
+}
+
+function letUsagiWear(){
+    usagi.style.backgroundImage ='url(assets/pajama.gif)'
+}
+
+function hachiware(){
+    usagi.style.backgroundImage ='url(assets/hachi.gif)'
+}
+
+function chiikawa(){
+    usagi.style.backgroundImage ='url(assets/chiii.gif)'
+}
